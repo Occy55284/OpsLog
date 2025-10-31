4aab1e149872d998cc6deae6668b1129c21861ed
Occy55284 <grahammark55284@gmail.com>
2025-10-31 11:42:24 +0000

    fix: correct supabaseAdmin relative path (../../lib)

--- web/pages/api/login-pin.js
+++ web/pages/api/login-pin.js
@@ -2,7 +2,8 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { serialize } from 'cookie';
-import { getSupabaseAdmin } from '../../../lib/supabaseAdmin';
+import { getSupabaseAdmin } from '../../lib/supabaseAdmin.js';
+
 
 const COOKIE_NAME = 'opslog_session';
 const MAX_AGE = 60 * 60 * 12; // 12 hours
