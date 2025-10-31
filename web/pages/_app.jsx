75ce7310edf0bd8319654f51b1b78d6f788abb5e
Occy55284 <grahammark55284@gmail.com>
2025-10-31 09:33:41 +0000

    correct _app.jsx import and syntax

--- web/pages/_app.jsx
+++ web/pages/_app.jsx
@@ -1,6 +1,9 @@
-import type { AppProps } from 'next/app';
+// pages/_app.jsx
 import '../styles/globals.css';
-export default function MyApp({ Component, pageProps }: AppProps) {
+
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+
 
